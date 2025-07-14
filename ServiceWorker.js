98e063da82d74f130547c840cd2ea3014fb8dfdb
Nawaz Ali <fojiphoto161@gmail.com>
2025-07-14 16:47:48 +0500

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'Golf-Orbit-v1';
 const urlsToCache = [
     '/',
     '/index.html',
-    '/Build/0cd7d42736f9840c7d2497bd194f6522.data.unityweb',
+    '/Build/267175e87e456c59c2addaff7e8d5b7b.data.unityweb',
     '/Build/f4ecbadad57c4ebc88341fe9350e277e.wasm.unityweb',
     '/Build/c5e3ce1a0242c704135be4d866484562.framework.js.unityweb',
     '/Build/9853637125e801e9aae48e78dbbdcfca.loader.js',
